a436642551e429594c47c8b30f682c450d480c18
lijinpeng1234 <jinpeng_ljp@163.com>
2019-10-15 20:12:28 +0800

    hellow git

---
+++ user/src/main/java/com/yyz/entity/User.java
@@ -0,0 +1,54 @@
+package com.yyz.entity;
+
+public class User {
+
+    private  int id;
+    private  String name;
+    private  String sex;
+    private  int age;
+
+
+    public void getId(int id) {
+        this.id = id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", sex='" + sex + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+
+}
